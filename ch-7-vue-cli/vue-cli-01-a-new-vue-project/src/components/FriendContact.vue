4a5e676bc5f932084460f5e22d0e4fd0966a6dbe
김진호 <skulter@naver.com>
2023-12-25 18:46:40 +0900

    컴포넌트 props

---
+++ ch-7-vue-cli/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,25 @@
+<template>
+    <li>
+        <h2>{{ name }}</h2>
+        <button @click="toggleShow">{{ isShow ? 'Hide' : 'Show' }} Details</button>
+        <ul v-if="isShow">
+            <li><strong>Phone:</strong>{{ phoneNumber }}</li>
+            <li><strong>Email:</strong>{{ emailAddress }}</li>
+        </ul>
+    </li>
+</template>
+<script>
+export default {
+    props: ['name', 'phoneNumber', 'emailAddress'],
+    data() {
+        return {
+            isShow: false,
+        }
+    },
+    methods: {
+        toggleShow() {
+            this.isShow = !this.isShow
+        }
+    }
+}
+</script>
